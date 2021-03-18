0e3c44a086319a10fb60018cb70927f92448d0f3
TheBigO <oliver.juwig@web.de>
2021-03-18 14:47:44 +0100

    Polishing for Release 2.6

---
+++ Docs/update-documentation.bat
@@ -0,0 +1,14 @@
+copy "Backlog.md" ..\..\Controller.Wiki\Backlog.md
+copy "Classes Reference.md" ..\..\Controller.Wiki\Classes-Reference.md
+copy "Constants Reference.md" ..\..\Controller.Wiki\Constants-Reference.md
+copy "Development Overview & Concepts.md" "..\..\Controller.Wiki\Development-Overview-&-Concepts.md"
+copy "Footer.md" ..\..\Controller.Wiki\_Footer.md
+copy "Home.md" ..\..\Controller.Wiki\Home.md
+copy "Installation & Configuration.md" "..\..\Controller.Wiki\Installation-&-Configuration.md"
+copy "Overview.md" ..\..\Controller.Wiki\Overview.md
+copy "Plugins & Modes.md" "..\..\Controller.Wiki\Plugins-&-Modes.md"
+copy "Releases.md" ..\..\Controller.Wiki\Release-Notes.md
+copy "Sidebar.md" ..\..\Controller.Wiki\_Sidebar.md
+copy "Updates.md" ..\..\Controller.Wiki\Update-Notes.md
+copy "Using Simulator Controller.md" ..\..\Controller.Wiki\Using-Simulator-Controller.md
+copy "Virtual Race Engineer.md" ..\..\Controller.Wiki\Virtual-Race-Engineer.md

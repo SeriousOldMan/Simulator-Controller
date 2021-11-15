9e00bafecdf408ca93520ba028b89059832e6cea
TheBigO <oliver.juwig@web.de>
2021-11-15 12:41:26 +0100

    Team Server Part 6

---
+++ Sources/Special/Team Server/Team Server Connector/TeamServerConnector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeamServerConnector {
+    public class TeamServerConnector {
+        TeamServerConnector(string url, string token = "") {
+
+        }
+
+        public string Login(string name, string password) {
+            return "";
+        }
+
+        public string Logout(string token) {
+            return "";
+        }
+    }
+}

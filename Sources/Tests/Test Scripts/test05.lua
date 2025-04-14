6ebfc6a15fea707a4b9dacb498ab28b47bfb3f18
TheBigO <oliver.juwig@web.de>
2025-04-14 14:45:30 +0200

    Foreign calls in Lua scripts Part 1

--- Sources/Tests/Test Scripts/test05.lua
+++ Sources/Tests/Test Scripts/test05.lua
@@ -2,6 +2,14 @@
 -- Test to see if we:
 -- * call foreign functions in AHK.
 
+function __normalizeArguments(arguments)
+	for i = 1, #arguments do
+		arguments[i] = tostring(arguments[i])
+	end
+	
+	return arguments
+end
+
 function foreign(name)
 	local handler = __Foreign(name)
 	

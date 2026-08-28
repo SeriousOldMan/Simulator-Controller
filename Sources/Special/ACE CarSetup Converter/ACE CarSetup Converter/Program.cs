1b7ba9680b903020ba320764ccecdd8c4245459f
TheBigO <oliver.juwig@web.de>
2026-08-28 13:54:25 +0200

    ACE Setup Editor Part 2

---
+++ Sources/Special/ACE CarSetup Converter/ACE CarSetup Converter/Program.cs
@@ -0,0 +1,41 @@
+﻿using ProtoBufJsonConverter;
+using ProtoBufJsonConverter.Models;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+
+namespace ACECarSetupConverter
+{
+    internal class Program
+    {
+        static string protoDefinition;
+
+        static void readSetup(string protoFile, string jsonFile)
+        {
+            var bytes = File.ReadAllBytes(protoFile);
+            var request = new ConvertToJsonRequest(protoDefinition, "ACEvo.CarSetup", bytes);
+
+            File.WriteAllText(jsonFile, new Converter().ConvertAsync(request).Result);
+        }
+
+        static void writeSetup(string jsonFile, string protoFile)
+        {
+            var json = File.ReadAllText(jsonFile);
+            var request = new ConvertToProtoBufRequest(protoDefinition, "ACEvo.CarSetup", json);
+
+            File.WriteAllBytes(protoFile, new Converter().ConvertAsync(request).Result);
+        }
+
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+
+            protoDefinition = File.ReadAllText(args[0]);
+
+            if (args[1].ToLower() == "-read")
+                readSetup(args[2], args[3]);
+            else if (args[0].ToLower() == "-write")
+                writeSetup(args[2], args[3]);
+        }
+    }
+}
---
+++ Sources/Special/ACE CarSetup Converter/ACE CarSetup Converter/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("ACE CarSetup Converter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACE CarSetup Converter")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("f946f968-5ce8-44ac-96c8-50f2c8e5fca8")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
